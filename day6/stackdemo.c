e30ac9011047c3e06396df29ae783858f5de2f3f
Aasaithambi Jay <47378496+aasaij@users.noreply.github.com>
2026-04-30 06:10:30 +0530

    Add files via upload

---
+++ day6/stackdemo.c
@@ -0,0 +1,12 @@
+//Program to implement stack operatins using array
+#include <stdio.h>
+//defining the stack
+typedef struct Stack{
+	int capacity;
+	int size;
+	int *elements;
+}stack;
+int main(){
+	
+	return 0;
+}
